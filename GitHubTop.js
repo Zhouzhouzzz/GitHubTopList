b63e180ad552070e59cb150400238356477e6939
Zhouzhouzzz <48087256+Zhouzhouzzz@users.noreply.github.com>
2019-09-07 17:25:52 +0800

    Update GitHubTop.js

--- GitHubTop.js
+++ GitHubTop.js
@@ -1,33 +1,16 @@
-// var Helper = {
-//     isToBottom: function ($viewport, $content) {
-//         return $viewport.height() + $viewport.scrollTop() + 20 > $content.height()
-//     }
-// }
-
-// var Paging = {
-//     init: function () {
-//         this.$tabs = $('footer>div');
-//         this.$pages = $('main>section');
-//         this.bind();
-//     },
-//     bind: function () {
-//         var _this = this;
-//         this.$tabs.on('click', function () {
-//             var $this = $(this);
-//             var index = $this.index();
-//             $this.addClass('active').siblings().removeClass('active')
-//             _this.$pages.eq(index).fadeIn().siblings().fadeOut()
-//         })
-//     }
-// }
-
-
-//获取repos的ajax请求
 var index = 1,
     pageRepos = 1,
-    pageUsers = 1;
+    pageUsers = 1,
+    pageSearch = 1,
+    _index = 1;
 
-function getTopRepos(pages, keyword, num) {
+var isLoading = false,
+    timeOut = 0;
+
+//获取repos的ajax请求
+function getTopRepos(pages, keyword, num, flag) {
+    if (isLoading) return;
+    isLoading = true;
     $.ajax({
         url: 'https://api.github.com/search/repositories?sort=stars&order=desc',
         dataType: 'json',
@@ -48,25 +31,39 @@ function getTopRepos(pages, keyword, num) {
                 </a>
                 <hr>`
                 var $node = $(tmp);
-                $node.find('p:nth-child(1)').text(index);
+                if (flag == 1) {
+                    $node.find('p:nth-child(1)').text(index);
+                    //console.log(index)
+                    index++;
+                } else {
+                    $node.find('p:nth-child(1)').text(_index);
+                    _index++;
+                }
                 $node.find('h2').text(item.name);
                 $node.find('p:nth-child(2)').text(item.description);
                 $node.find('p:nth-child(3)>span:nth-child(1)').text(item.stargazers_count);
-                index++;
+
                 $('section').eq(num).append($node);
             });
+            if (flag == 1) {
+                pageRepos++;
+            } else {
+                pageSearch++;
+            }
 
-            pageRepos++;
         },
         error: function () {
-            alert('error!');
+            console.log('error!');
         }
+    }).always(function () {
+        isLoading = false;
     })
 }
 
 //获取user的ajax请求
-
 function getTopUser() {
+    if (isLoading) return;
+    isLoading = true;
     $.ajax({
         url: 'https://api.github.com/search/users?q=followers:>1000+language:javascript',
         dataType: 'json',
@@ -85,58 +82,77 @@ function getTopUser() {
                 $node.find('img').prop('src', item.avatar_url);
                 $node.find('h2').text(item.login);
                 $('section').eq(1).append($node);
-                pageUsers++;
             });
+            pageUsers++;
 
         },
         error: function () {
             alert('error!');
         }
+    }).always(function () {
+        isLoading = false;
     })
 }
+//tab以及展示面板的设置
+function addActive(ind) {
+    $('footer div:nth-child(' + (ind + 1) + ')').addClass('active').siblings().removeClass('active');
+    $('main section').eq(ind).addClass('active').siblings().removeClass('active');
+}
 
-// getTopUser();
-//getTopRepos(pageSearch, 'console.log()', 2);
-getTopRepos(pageRepos, 'language:javascript', 0);
+//界面滚动函数
 
 
 
-$('footer div:first-child').click(function () {
-    $(this).addClass('active').siblings().removeClass('active')
-    $('main section').eq(0).fadeIn().siblings().fadeOut()
-    getTopRepos(pageRepos, 'language:javascript', 0);
+function scrollFun(handler, ind) {
+    if ($('main').scrollTop() + $('main').height() == $('section').eq(ind).height()) {
+        if (timeOut == 0) {
+            timeOut = setTimeout(handler, 1000);
+        } else {
+            clearTimeout(timeOut);
+            timeOut = 0;
+        }
+    }
+}
+
+
+//界面初始加载
+getTopRepos(pageRepos, 'language:javascript', 0, 1);;
+
+//tabs的点击事件
+$('footer div').click(function (e) {
+    if ($(e.target).html() == 'Top repos' || ($(e.target).siblings().html() == 'Top repos')) {
+        addActive(0);
+        getTopRepos(pageRepos, 'language:javascript', 0, 1);
+    } else if ($(e.target).html() == 'Top users' || ($(e.target).siblings().html() == 'Top users')) {
+        addActive(1);
+        getTopUser();
+    } else {
+        addActive(2);
+
+    }
 })
 
-$('footer div:nth-child(2)').click(function () {
-    $(this).addClass('active').siblings().removeClass('active')
-    $('main section').eq(1).fadeIn().siblings().fadeOut()
-    getTopUser();
-})
-
-$('footer div:nth-child(3)').click(function () {
-    $(this).addClass('active').siblings().removeClass('active')
-    $('main section').eq(2).fadeIn().siblings().fadeOut()
-    getTopUser();
-})
 $('.search-btn').click(function () {
-    getTopRepos(pageRepos, $('#repo').val(), 2);
+    getTopRepos(pageRepos, $('#repo').val(), 2, 3);
 })
 
-var timeOut = 0;
 $('main').scroll(function () {
-    if ($('section').eq(0).hasClass('active')) {
-        if ($('main').scrollTop() + $('main').height() == $('section').eq(0).height()) {
-            var timeOut = setTimeout(getTopRepos(pageRepos, 'language:javascript', 0), 1000)
-        }
-    }
-    if ($('section').eq(1).hasClass('active')) {
-        if ($('main').scrollTop() + $('main').height() == $('section').eq(1).height()) {
-            getTopUser();
-        }
-    }
-    if ($('section').eq(2).hasClass('active')) {
-        if ($('main').scrollTop() + $('main').height() == $('section').eq(2).height()) {
-            getTopRepos(pageRepos, $('#repo').val(), 2);
-        }
+    var indexScr = $('section.active').index();
+    switch (indexScr) {
+        case 0:
+            scrollFun(function () {
+                getTopRepos(pageRepos, 'language:javascript', 0, 1)
+            }, 0);
+            break;
+        case 1:
+            scrollFun(function () {
+                getTopUser()
+            }, 1);
+            break;
+        case 2:
+            scrollFun(function () {
+                getTopRepos(pageRepos, $('#repo').val(), 2, 3);
+            }, 2);
+            break;
     }
 })
